a7bfbcbc0ab53de11c8291ade9ce9c482f7dfe4d
William Eriksson <william99eriksson@outlook.com>
2025-04-19 17:43:33 +0200

    Create role nu möjligt måste addera ett sätt att tilddela roller också

---
+++ FootballScoreApi/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using FootballScoreApp.Entities;
+using FootballScoreApp.Features.Roles.CreateRole;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballScoreApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : Controller
+    {
+        private readonly ISender _sender;
+
+        public AdminController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("CreateRole")]
+        public async Task<ActionResult<Role?>> CreateRole(CreateRoleCommand command)
+        {
+            var role = await _sender.Send(command);
+
+            if(role is null)
+            {
+                BadRequest("Role already exists");
+            }
+
+            return Ok(role);
+        }
+    }
+}
---
+++ FootballScoreApi/Features/Roles/CreateRole/CreateRoleCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using FootballScoreApp.DbConnection;
+using FootballScoreApp.Entities;
+using MediatR;
+
+namespace FootballScoreApp.Features.Roles.CreateRole
+{
+    public class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, Role?>
+    {
+        private readonly AppDbContext _context;
+        public CreateRoleCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Role?> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+        {
+            var role = new Role
+            {
+                Name = request.name
+            };
+            if (role is null)
+            {
+                return null;
+            }
+
+            await _context.AddAsync(role);
+            await _context.SaveChangesAsync();
+
+            return role;
+        }
+    }
+}
